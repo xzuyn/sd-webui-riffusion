a4b0b465365e82b9a64bd7c489c641c1235dd727
xzuyn <16216325+xzuyn@users.noreply.github.com>
2024-03-12 15:52:23 -0400

    Fixes
    1. Fixes problem with `max_iter` not existing in newer versions of torchaudio.
    2. Fixes `torch.linalg.lstsq` not working with non-CUDA devices by selecting `cpu` if using CPU or ROCm.
    3. Black formatting.

--- scripts/riffusion.py
+++ scripts/riffusion.py
@@ -188,11 +188,9 @@ class RiffusionScript(scripts.Script):
             n_fft=n_fft,
             hop_length=hop_length,
             win_length=win_length,
-            num_samples=num_samples,
             sample_rate=sample_rate,
             mel_scale=True,
             n_mels=n_mels,
-            max_mel_iters=200,
             num_griffin_lim_iters=32,
         )
 
@@ -275,19 +273,24 @@ class RiffusionScript(scripts.Script):
         n_fft: int,
         hop_length: int,
         win_length: int,
-        num_samples: int,
         sample_rate: int,
         mel_scale: bool = True,
         n_mels: int = 512,
-        max_mel_iters: int = 200,
         num_griffin_lim_iters: int = 32,
-        device: str = platform.system() == "Darwin" and "cpu" or "cuda:0",
+        device: str = None,
     ) -> np.ndarray:
         """
         Reconstruct a waveform from a spectrogram.
         This is an approximate inverse of spectrogram_from_waveform, using the Griffin-Lim algorithm
         to approximate the phase.
         """
+        if device is None:
+            device = (
+                "cuda:0"
+                if torch.cuda.is_available() and torch.version.cuda is not None
+                else "cpu"
+            )
+
         Sxx_torch = torch.from_numpy(Sxx).to(device)
 
         # TODO(hayk): Make this a class that caches the two things
@@ -301,7 +304,6 @@ class RiffusionScript(scripts.Script):
                 n_stft=n_fft // 2 + 1,
                 norm=None,
                 mel_scale="htk",
-                max_iter=max_mel_iters,
             ).to(device)
 
             Sxx_torch = mel_inv_scaler(Sxx_torch)
@@ -319,14 +321,15 @@ class RiffusionScript(scripts.Script):
         return waveform
 
 
-def convert_audio_file(image, output_dir, crop_width = None):
+def convert_audio_file(image, output_dir, crop_width=None):
     image_file = Image.open(image)
     return convert_audio_image(image, image_file, output_dir, crop_width)
 
-def convert_audio_image(image, image_file, output_dir, crop_width = None):
+
+def convert_audio_image(image, image_file, output_dir, crop_width=None):
     if crop_width is not None and crop_width < image_file.width:
-        image_file = image_file.crop((0,0,crop_width,image_file.height))
-    
+        image_file = image_file.crop((0, 0, crop_width, image_file.height))
+
     new_filename = os.path.splitext(os.path.basename(image))[0] + ".wav"
     filename = os.path.join(output_dir, new_filename)
     riffusion = RiffusionScript()
@@ -336,17 +339,19 @@ def convert_audio_image(image, image_file, output_dir, crop_width = None):
         f.write(wav_bytes.getbuffer())
     return filename
 
+
 def convert_audio(
     image_dir: str,
     file_regex: str,
     join_images: bool,
     crop_method: str,
     crop_width: int,
-    rhythm:int,
+    rhythm: int,
     band_start: float,
     band_length: float,
     threshold_offset: float,
-    ignore_range: float) -> None:
+    ignore_range: float,
+) -> None:
 
     images = get_image(file_regex, image_dir)
 
@@ -357,10 +362,19 @@ def convert_audio(
         image_file = Image.open(image)
         if crop_method == "Fixed":
             width = crop_width
-        elif crop_method.startswith("Beat Finder") and (not crop_method.endswith("(Once)") or i == 0):
-            width = find_cutoff(image_file, rhythm, band_start, band_length, threshold_offset, ignore_range)["cutoff"]
+        elif crop_method.startswith("Beat Finder") and (
+            not crop_method.endswith("(Once)") or i == 0
+        ):
+            width = find_cutoff(
+                image_file,
+                rhythm,
+                band_start,
+                band_length,
+                threshold_offset,
+                ignore_range,
+            )["cutoff"]
             print("Cutoff found at:", width)
-        
+
         output_files.append(convert_audio_image(image, image_file, image_dir, width))
 
     if join_images and len(output_files) > 1:
@@ -381,26 +395,38 @@ def convert_audio(
 
     print(f"Converted {len(images)} images to audio")
 
-def test_beat_finder(image_dir: str,
+
+def test_beat_finder(
+    image_dir: str,
     file_regex: str,
-    rhythm:int,
+    rhythm: int,
     band_start: float,
     band_length: float,
     threshold_offset: float,
-    ignore_range: float) -> Image:
+    ignore_range: float,
+) -> Image:
 
     images = get_image(file_regex, image_dir)
-    if (len(images) == 0):
+    if len(images) == 0:
         return None
-    
+
     image = images[0]
     image_file = Image.open(image).convert("RGB")
-    output = Image.new(mode="RGB", size=(image_file.width, image_file.height  + 256), color=(255,255,255))
+    output = Image.new(
+        mode="RGB",
+        size=(image_file.width, image_file.height + 256),
+        color=(255, 255, 255),
+    )
     output.paste(image_file)
-    beat_finder_result = find_cutoff(image_file, rhythm, band_start, band_length, threshold_offset, ignore_range)
+    beat_finder_result = find_cutoff(
+        image_file, rhythm, band_start, band_length, threshold_offset, ignore_range
+    )
     draw = ImageDraw.Draw(output, "RGBA")
-    register_rect = [(0, beat_finder_result["register_start"]), (image_file.width, beat_finder_result["register_end"])]
-    
+    register_rect = [
+        (0, beat_finder_result["register_start"]),
+        (image_file.width, beat_finder_result["register_end"]),
+    ]
+
     level_range = beat_finder_result["level_range"]
     if level_range > 0:
         min_level = beat_finder_result["min_level"]
@@ -408,20 +434,28 @@ def test_beat_finder(image_dir: str,
         scale = 255 / level_range
         for i, value in enumerate(beat_finder_result["one_line"]):
             line_height = (max_level - value) * scale
-            draw.line([(i, output.height), (i, output.height - line_height)], fill=(0,0,0))
+            draw.line(
+                [(i, output.height), (i, output.height - line_height)], fill=(0, 0, 0)
+            )
             pass
 
         scaled_threshold = (max_level - beat_finder_result["threshold"]) * scale
-        draw.line([(0, output.height - scaled_threshold), (output.width, output.height - scaled_threshold)], fill=(0,255,0))
-    
+        draw.line(
+            [
+                (0, output.height - scaled_threshold),
+                (output.width, output.height - scaled_threshold),
+            ],
+            fill=(0, 255, 0),
+        )
+
     draw.rectangle(register_rect, fill=(0, 0, 255, 64))
     for beat in beat_finder_result["above_threshold"]:
-        draw.line([(beat, 0), (beat, output.height)], fill=(255,0,0), width= 2)
-    
+        draw.line([(beat, 0), (beat, output.height)], fill=(255, 0, 0), width=2)
+
     cutoff = beat_finder_result["cutoff"]
     if cutoff != None:
-        draw.line([(cutoff, 0), (cutoff, output.height)], fill=(255,165,0), width= 2)
-    
+        draw.line([(cutoff, 0), (cutoff, output.height)], fill=(255, 165, 0), width=2)
+
     del draw
 
     return output
@@ -432,33 +466,41 @@ def get_image(file_regex, image_dir):
     globs = map(lambda x: x.strip(), file_regex.split(","))
     for g in globs:
         images.extend(glob.glob(os.path.join(image_dir, g)))
-    
+
     images.sort()
 
     return images
 
-def find_cutoff(image, rhythm = 4, band_start = 0.25, band_length = 0.75, threshold_offset = 0.75, ignore_range = 0.05):
-    
-    result = { "cutoff": None }
+
+def find_cutoff(
+    image,
+    rhythm=4,
+    band_start=0.25,
+    band_length=0.75,
+    threshold_offset=0.75,
+    ignore_range=0.05,
+):
+
+    result = {"cutoff": None}
 
     ignore_distance = image.width * ignore_range
     register_start = int(image.height * band_start)
     register_lenght = int(image.height * band_length)
     register_end = register_start + register_lenght
-    
+
     result["register_start"] = register_start
     result["register_end"] = register_end
 
     band = image.crop((0, register_start, image.width, register_end))
-    gray_image = band.convert('L')
+    gray_image = band.convert("L")
     one_line = list(gray_image.resize((gray_image.width, 1)).getdata())
 
     result["one_line"] = one_line
 
-    #one_pixel = list(gray_image.resize((1, 1)).getdata())
-    #average = one_pixel[0]
+    # one_pixel = list(gray_image.resize((1, 1)).getdata())
+    # average = one_pixel[0]
     min_level = min(one_line)
-    max_level = max(one_line) 
+    max_level = max(one_line)
     level_range = max_level - min_level
     result["min_level"] = min_level
     result["max_level"] = max_level
@@ -467,9 +509,11 @@ def find_cutoff(image, rhythm = 4, band_start = 0.25, band_length = 0.75, thresh
     result["threshold"] = threshold
     above_threshold = []
     for i, value in enumerate(one_line):
-        if value < threshold and (len(above_threshold) == 0 or i - above_threshold[-1] > ignore_distance):
+        if value < threshold and (
+            len(above_threshold) == 0 or i - above_threshold[-1] > ignore_distance
+        ):
             above_threshold.append(i)
-    
+
     result["above_threshold"] = above_threshold
 
     if len(above_threshold) == 0:
@@ -484,8 +528,8 @@ def find_cutoff(image, rhythm = 4, band_start = 0.25, band_length = 0.75, thresh
             continue
 
         distances.append(value - above_threshold[i - 1])
-    
-    if (len(distances) == 0):
+
+    if len(distances) == 0:
         return result
 
     result["distances"] = distances
@@ -504,6 +548,7 @@ def find_cutoff(image, rhythm = 4, band_start = 0.25, band_length = 0.75, thresh
     cutoff = int(beat_count * distance)
     result["cutoff"] = cutoff
     return result
+
 
 def on_ui_tabs():
     with gr.Blocks() as riffusion_ui:
@@ -531,10 +576,15 @@ def on_ui_tabs():
 
                 crop_method = gr.Dropdown(
                     label="Crop method",
-                    choices=["None", "Fixed", "Beat Finder (Once)", "Beat Finder (Every)"],
+                    choices=[
+                        "None",
+                        "Fixed",
+                        "Beat Finder (Once)",
+                        "Beat Finder (Every)",
+                    ],
                     value="None",
                     interactive=True,
-                    allow_custom_value=False
+                    allow_custom_value=False,
                 )
                 with gr.Column(visible=False) as fixed_block:
                     crop_width = gr.Number(
@@ -602,12 +652,14 @@ def on_ui_tabs():
                                 band_start,
                                 band_length,
                                 threshold_offset,
-                                ignore_range
+                                ignore_range,
                             ],
                             outputs=[beat_finder_image],
                         )
-                
-                crop_method.change(on_crop_method_change, crop_method, [fixed_block,beat_finder_block])
+
+                crop_method.change(
+                    on_crop_method_change, crop_method, [fixed_block, beat_finder_block]
+                )
             with gr.Column(variant="panel"):
                 with gr.Row():
                     convert_folder_btn = gr.Button(
@@ -625,22 +677,33 @@ def on_ui_tabs():
                             band_start,
                             band_length,
                             threshold_offset,
-                            ignore_range
+                            ignore_range,
                         ],
                         outputs=[],
                     )
                 gr.HTML(value="<p>Converts all images in a folder to audio</p>")
     return ((riffusion_ui, "Riffusion", "riffusion_ui"),)
 
-def on_beat_finder_test_click(image_dir: str,
+
+def on_beat_finder_test_click(
+    image_dir: str,
     file_regex: str,
-    rhythm:int,
+    rhythm: int,
     band_start: float,
     band_length: float,
     threshold_offset: float,
-    ignore_range: float):
+    ignore_range: float,
+):
 
-    image = test_beat_finder(image_dir, file_regex, rhythm, band_start, band_length, threshold_offset, ignore_range)
+    image = test_beat_finder(
+        image_dir,
+        file_regex,
+        rhythm,
+        band_start,
+        band_length,
+        threshold_offset,
+        ignore_range,
+    )
     return gr.update(value=image)
 
 
@@ -649,5 +712,5 @@ def on_crop_method_change(crop_method):
     beat_finder_visible = True if crop_method.startswith("Beat Finder") else False
     return gr.update(visible=fixed_visible), gr.update(visible=beat_finder_visible)
 
-script_callbacks.on_ui_tabs(on_ui_tabs)
 
+script_callbacks.on_ui_tabs(on_ui_tabs)
